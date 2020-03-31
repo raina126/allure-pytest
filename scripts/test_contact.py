ca617a9dc060c1d2149aa6fd63bb4f0531a34453
raina121 <raina121@126.com>
2020-03-31 21:29:37 +0800

    first commit

---
+++ scripts/test_contact.py
@@ -0,0 +1,30 @@
+import pytest
+from appium import webdriver
+import allure
+
+class TestContact:
+
+    @allure.step(title='登录的测试脚本')
+    def test_login(self):
+
+        assert 1
+
+    @allure.step(title='用户名的测试脚本')
+    def test_username(self):
+
+        assert 1
+
+    @allure.step(title='密码的测试脚本')
+    def test_password(self):
+
+        assert 1
+
+
+
+
+
+
+
+
+
+
